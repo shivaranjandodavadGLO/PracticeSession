669667bc20d5839319bf93111ad3c818251610e8
shivaranjan.dodavad <shivaranjan.dodavad@globallogic.com>
2025-03-27 18:42:31 +0530

    Project Structure

---
+++ order2/src/main/java/com/hackthon/order2/model/Order.java
@@ -0,0 +1,30 @@
+package com.hackthon.order2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    String orderId;
+    String userId;
+    String total_amount;
+    String paymentStatus;
+    LocalDate created_at;
+    String order_item_id;
+    String product_id;
+    String quantity;
+    String price;
+}
