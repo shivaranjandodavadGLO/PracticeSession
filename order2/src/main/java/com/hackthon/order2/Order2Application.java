ca1e5889547a5485be75baf41376d72234e51c8d
shivaranjan.dodavad <shivaranjan.dodavad@globallogic.com>
2025-03-27 18:16:00 +0530

    commit

---
+++ order2/src/main/java/com/hackthon/order2/Order2Application.java
@@ -0,0 +1,13 @@
+package com.hackthon.order2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Order2Application.class, args);
+	}
+
+}
---
+++ order2/src/test/java/com/hackthon/order2/Order2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthon.order2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Order2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
